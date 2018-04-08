4d6877d0a2e7320d238a32036315b7b0f54c6816
Victor Gomez <victor@medicmobile.org>
2018-04-08 08:45:41 -0600

    Initial commit

---
+++ __tests__/pouch.js
@@ -0,0 +1,40 @@
+import PouchDB from 'pouchdb'
+import Couch from '../lib/pouch'
+import docs from './docs.json'
+
+describe('couch', () => {
+  const DB_NAME = 'couch-test'
+  const COUCH_URL = `http://admin:pass@localhost:5984/${DB_NAME}`
+  const couch = new Couch(COUCH_URL)
+
+  const cleanUp = async () => await new PouchDB(COUCH_URL).destroy()
+
+  beforeEach(async () => cleanUp())
+  afterEach(() => cleanUp())
+
+  describe('couch operations', () => {
+    beforeEach(async () => await new PouchDB(COUCH_URL).bulkDocs(docs))
+
+    test('empty constructor', () => {
+      expect(() => new Couch()).toThrowError(/Missing Parameter/)
+      expect.assertions(1)
+    })
+
+    test('docs, count, sortedDocs', async () => {
+      expect((await couch.docs())[0].doc.rev).toBe(docs[0].doc.rev)
+      expect((await couch.count()) === 2).toBe(true)
+      expect((await couch.sortedDocs())[0].doc.rev).toBe(docs[1].doc.rev)
+    })
+
+    test('docs by keys', async () => {
+      const saved = (await couch.docs())[0]
+      const doc = (await couch.docs([saved._id]))[0]
+      expect(doc.id).toEqual(saved.id)
+    })
+
+    test('getChanges limit 10', async () => {
+      const changes = await couch.changes(10, 0)
+      expect(changes.length).toBe(2)
+    })
+  })
+})
